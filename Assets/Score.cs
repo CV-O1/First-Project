4e2ddfc05b5e4709594abcc78ddc58a014b2ad4d
CV-01 <sofiaalvarez1307@gmail.com>
2024-06-18 12:16:16 -0400

    Initial commit

---
+++ Assets/Score.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public int score = 0;
+    public float timer = 10f;
+
+    // AddScore increments the value of score by 1.
+    public void AddScore()
+    {
+       
+        if (timer > 0f)
+        {
+            score++;
+            print("Score: " + score);
+        }
+
+        else
+        {
+            print("Out of time!");
+        }
+
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+    }
+
+}
